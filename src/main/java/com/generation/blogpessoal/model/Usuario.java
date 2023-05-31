9a0486bc010562c101ac3a5fa14788ffc237cc92
Rafael <raaf.marques019@gmail.com>
2023-05-31 15:03:09 -0300

    Atualização - Blog Pessoal

--- src/main/java/com/generation/blogpessoal/model/Usuario.java
+++ src/main/java/com/generation/blogpessoal/model/Usuario.java
@@ -18,30 +18,38 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 @Entity
-@Table(name ="tb_usuarios")
+@Table(name = "tb_usuarios")
 public class Usuario {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	@NotNull(message = "O atributo NOME é obrigatório")
 	private String nome;
-	
+
 	@NotNull(message = "O atributo Usuario é obrigatório")
 	@Email(message = "O atributo usuario vai receber um e-mail válido")
 	private String usuario;
-	
+
 	@NotBlank(message = "O atributo senha é obrigatório")
 	@Size(min = 8, message = "A senha tem que ter no mínimo 8 caracteres")
 	private String senha;
-	
-	@Size(max = 5000,message = "O link da foto não deve ser maior que 5000 caracteres")
+
+	@Size(max = 5000, message = "O link da foto não deve ser maior que 5000 caracteres")
 	private String foto;
-	
-	@OneToMany(fetch = FetchType.LAZY,mappedBy = "usuario",cascade = CascadeType.REMOVE)
+
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "usuario", cascade = CascadeType.REMOVE)
 	@JsonIgnoreProperties("usuario")
 	private List<Postagem> postagem;
+
+	public Usuario(Long id, String nome, String usuario, String senha, String foto) {
+		this.id = id;
+		this.nome = nome;
+		this.usuario = usuario;
+		this.senha = senha;
+		this.foto = foto;
+	}
 
 	public Long getId() {
 		return id;
@@ -90,6 +98,5 @@ public class Usuario {
 	public void setPostagem(List<Postagem> postagem) {
 		this.postagem = postagem;
 	}
-	
-	
+
 }
