21d29013768bb5436e9fdb3fc7162308cde2f9e9
Rafael <raaf.marques019@gmail.com>
2023-06-26 12:26:40 -0300

    Swagger Update - Blog Pessoal

--- src/main/java/com/generation/blogpessoal/configuration/SwaggerConfig.java
+++ src/main/java/com/generation/blogpessoal/configuration/SwaggerConfig.java
@@ -19,18 +19,18 @@ public class SwaggerConfig {
         return new OpenAPI()
                 .info(new Info()
                         .title("Projeto Blog Pessoal")
-                        .description("Projeto Blog Pessoal - Felipe Sousa")
+                        .description("Projeto Blog Pessoal - Rafael Marques")
                         .version("v0.0.1")
                         .license(new License()
                                 .name("Generation Brasil")
                                 .url("https://brazil.generation.org/"))
                         .contact(new Contact()
-                                .name("Felipe Sousa")
-                                .url("https://github.com/FelipeSdsilva/")
-                                .email("felipe.fps09@hotmail.com")))
+                                .name("Rafa Marques")
+                                .url("https://github.com/dysrrafaa/")
+                                .email("raaf.marques019@gmail.com")))
                 .externalDocs(new ExternalDocumentation()
                         .description("Github")
-                        .url("https://github.com/FelipeSdsilva/"));
+                        .url("https://github.com/dysrrafaa/"));
     }
 
 
